4269146bd1c396c0022ce718074a9577a18b4897
rock-tran <rock.tran@manadr.com>
2023-09-04 14:04:17 +0700

    First commit

--- src/hello.go
+++ src/hello.go
@@ -1,8 +1,16 @@
 package main
 
 import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-  fmt.Println("Hello, world.")
+	fmt.Println("Hello, world.")
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
 
+	})
+	r.Run()
 }
