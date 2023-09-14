4c252b5b60984b046164a8a4483994a38b97e254
rock-tran <rock.tran@manadr.com>
2023-09-14 16:52:13 +0700

    Create method of User struct

--- src/domain/users/user.go
+++ src/domain/users/user.go
@@ -1,9 +1,32 @@
 package users
 
+import (
+	"rock/test_gin/utils/errors"
+	"strings"
+)
+
 type User struct {
 	Id          int64  `json:"id"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
 	DateCreated string `json:"date_created"`
+}
+
+func Validate(user *User) *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
+}
+
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
 }
--- src/services/users_service.go
+++ src/services/users_service.go
@@ -1,17 +1,14 @@
 package services
 
 import (
-	"net/http"
 	"rock/test_gin/domain/users"
 	"rock/test_gin/utils/errors"
 )
 
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
-  return nil, nil
-	
-  return &user, nil
-
-	return &user, &errors.RestErr{
-		Status: http.StatusInternalServerError,
+	if err := user.Validate(); err != nil {
+		return nil, err
 	}
+
+	return nil, nil
 }
